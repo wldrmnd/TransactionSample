a457f3b51ccde7ab13a23324c20201245c17076a
Egor Barkovsky <woldermind@gmail.com>
2021-02-12 19:35:53 +0300

    initial commit

---
+++ src/Client.java
@@ -0,0 +1,14 @@
+//Transaction:
+//ID CardHolder_Name CardHolder_Surname Amount Time_Begin Time_End
+
+//разработка всегда(!) начинается с проектирования
+//классов нижних(классов, которые ни от кого не зависят) уровней
+//add -> commit -> push
+
+//1. Git (сверху) -> Enable VSC
+
+public class Client {
+    public static void main(String[] args) {
+        System.out.println("Коммит");
+    }
+}

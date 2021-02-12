65298d26b44860140254d82753742406b6b11e22
Egor Barkovsky <woldermind@gmail.com>
2021-02-12 20:27:53 +0300

    calendar :)

--- src/entity/TransactionTime.java
+++ src/entity/TransactionTime.java
@@ -1,9 +1,15 @@
 package entity;
 
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+//загуглите что такое класс GregorianCalendar
+//в нем есть метод set(принимает он год, месяц, число, час, минуты, секунды)
+//считать все это должны в меню, потом передать сервис, потом в репо
 public class TransactionTime {
 
-    private String startTime;
-    private String endTime;
+    private GregorianCalendar startTime;
+    private Calendar endTime;
 
 
 }
